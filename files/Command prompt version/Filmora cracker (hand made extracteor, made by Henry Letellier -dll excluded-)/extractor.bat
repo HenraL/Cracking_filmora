210436fcf1cff0a408d3542acc7a91badddc15c6
Gitlab_test_project <letellier.henry@gmail.com>
2022-03-23 20:34:30 +0100

    Update extractor.bat

--- files/Command prompt version/Filmora cracker (hand made extracteor, made by Henry Letellier -dll excluded-)/extractor.bat
+++ files/Command prompt version/Filmora cracker (hand made extracteor, made by Henry Letellier -dll excluded-)/extractor.bat
@@ -2,9 +2,12 @@ color 0A
 echo off
 cls
 :start
+echo This Program will work for versions 9 and below
 echo If you encounter problems like acces denied, try running the program with admin wrights.
 echo If problems persist visit the debug section.
-echo Do you wish to legaly crack filmora 9? [(y)es/(n)o/(d)ebug]:
+echo Enter the version of your copy of filmora (9, 8, etc... not 9.2 or 8.4):
+set /p versionOfProg=
+echo Do you wish to legaly crack filmora %versionOfProg%? [(y)es/(n)o/(d)ebug]:
 set /p choice=
 if %choice%==y goto y
 if %choice%==Y goto y
@@ -18,11 +21,18 @@ goto start
 :y
 echo Extracting files.....
 cd files
+echo first atempt.
 echo on
-REM Below if the first line to modify if needed
-copy PYG64.dll "c:\Program Files\Wondershare\Filmora9"
+REM Below is the first line to modify if needed
+copy PYG64.dll "c:\Program Files\Wondershare\Filmora%versionOfProg%"
 REM Below is the second line to modify if needed
-copy winmm.dll "c:\Program Files\Wondershare\Filmora9"
+copy winmm.dll "c:\Program Files\Wondershare\Filmora%versionOfProg%"
+REM second atempt.
+REM Below is the first line to modify if needed
+copy PYG64.dll "c:\Program Files\Wondershare\Wondershare Filmora"
+REM Below is the second line to modify if needed
+copy winmm.dll "c:\Program Files\Wondershare\Wondershare Filmora"
+
 echo off
 echo Files extracted
 pause
@@ -60,7 +70,7 @@ echo "|=========================================================================
 echo "|                                      The python version (better develloped, better guided):                        |"
 echo "|                                      https://bit.ly/2No5Bzh                                                        |"
 echo "|____________________________________________________________________________________________________________________|"
-echo If the copy failes please check the following ellemnts:
+echo If the copy failes please check the following elements:
 echo 1. Check that you have all the required files
 echo 1.1 This means you have
 echo 1.1.1 This file
@@ -68,12 +78,12 @@ echo 1.1.2 A folder named files
 echo 1.1.3 In this folder a file named PYG64.dll (depending on your settings you will or not see the extension .dll)
 echo 1.1.4 In this same folder a file named winmm.dll (depending on your settings you will or not see the extension .dll)
 echo.
-echo If these previous steps are validated checke the second steps.
+echo If these previous steps are validated check the second steps.
 echo.
-echo 2. Check that you're current disk is labeled (C:) If this is no the case replace the letter c (in the line "c:\Program Files\Wondershare\Filmora9" by the lowercase letter of your disk in which the program is)
+echo 2. Check that you're current disk is labeled (C:) If this is no the case replace the letter c (in the line "c:\Program Files\Wondershare\Filmora%versionOfProg%" by the lowercase letter of your disk in which the program is)
 echo 2.1 Check that the folder "Wondershare" is located in The folder "Program Files" if this is not the case please change "Program Files" by the folder the folder in which the program is.
 echo.
-echo PS: To find the folder, wrigt click on the icon on your desktop --> reveal in explorer, repeat this action with the icon in the folder in which you'lle be if you do not see more than the shortcuts to Uninstall Wondershare Filmora9 and Wondershare Filmora9.
+echo PS: To find the folder, wrigt click on the icon on your desktop --> reveal in explorer, repeat this action with the icon in the folder in which you'll be if you do not see more than the shortcuts to Uninstall Wondershare Filmora%versionOfProg% and Wondershare Filmora%versionOfProg%.
 echo.
 echo If all these requirements are fufiled you should not encounter anymore problems in this software.
 echo.
